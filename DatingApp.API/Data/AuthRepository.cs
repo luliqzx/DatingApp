12c6a5278d9a9ad89e9c134667aa4459bb08fec9
Putra <lyputra@zuelligpharma.com>
2019-05-10 14:52:18 +0700

    Useregister DTO & Auth

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data {
     public class AuthRepository : IAuthRepository {
@@ -10,8 +11,28 @@ namespace DatingApp.API.Data {
         public AuthRepository (DataContext context) {
             _context = context;
         }
-        public Task<User> Login (string username, string password) {
-            throw new System.NotImplementedException ();
+        public async Task<User> Login (string username, string password) {
+            var user = await _context.Users.FirstOrDefaultAsync (x => x.Username == username);
+            if (user == null) {
+                return null;
+            }
+
+            if (!VerifyPasswordHash (password, user.PasswordHash, user.PasswordSalt)) {
+                return null;
+            }
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash (string password, byte[] passwordHash, byte[] passwordSalt) {
+
+            using (var hmac = new HMACSHA512 (passwordSalt)) {
+                var computeHash = hmac.ComputeHash (Encoding.UTF8.GetBytes (password));
+                for (int i = 0; i < computeHash.Length; i++) {
+                    if (computeHash[i] != passwordHash[i]) return false;
+                }
+                return true;
+            }
         }
 
         public async Task<User> Register (User user, string password) {
@@ -34,8 +55,10 @@ namespace DatingApp.API.Data {
             }
         }
 
-        public Task<bool> UserExists (string username) {
-            throw new System.NotImplementedException ();
+        public async Task<bool> UserExists (string username) {
+            if (await _context.Users.AnyAsync (x => x.Username == username))
+                return true;
+            return false;
         }
     }
 }
