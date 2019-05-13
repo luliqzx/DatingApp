ff1534d91c976065258671e3c966ba411cf301a7
Putra <lyputra@zuelligpharma.com>
2019-05-13 14:28:33 +0700

    Adding Login + Jwt

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos {
     public class UserForRegisterDto {
+        [Required]
         public string Username { get; set; }
+
+        [Required][
+            StringLength (8, MinimumLength = 4, ErrorMessage = "Please specify your password between 4 and 8 characters")
+        ]
         public string Password { get; set; }
     }
 }
