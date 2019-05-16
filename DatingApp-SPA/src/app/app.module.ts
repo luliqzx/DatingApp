d5b9255c2deb1eb717f207554092445c808ab421
Putra <lyputra@zuelligpharma.com>
2019-05-16 14:34:15 +0700

    v39-v41

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
   declarations: [AppComponent, ValueComponent, NavComponent],
   imports: [BrowserModule
     , HttpClientModule, FormsModule],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
